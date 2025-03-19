a88fb98576cbf154c2031e6c4d59472a7e29124a
Yasuyoshi Sako <yasuyoshi.sako@gmail.com>
2025-03-19 19:44:50 +0900

    Update Util.pm
    関数wrap_text_in_charsとwrap_text_in_wordsを追加

--- lib/Text/TinyTemplate/Util.pm
+++ lib/Text/TinyTemplate/Util.pm
@@ -3,7 +3,7 @@ package Text::TinyTemplate::Util;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # https://dankogai.livedoor.blog/archives/50940023.htmlを引用
 my %escape_rule = ( '&' => 'amp', '>' => 'gt', '<' => 'lt', '"' => 'quot', "'" => '#39' );
@@ -17,6 +17,37 @@ sub escape_html {
   $str =~ s/($escape_keys)(?!amp;)/'&' . $escape_rule{$1} . ';'/ge;
   $str;
 
+}
+
+sub wrap_text_in_chars {
+  my ($text, $max_length, $separator) = @_;
+  my $wrapped_text;
+
+  while (length($text) > $max_length) {
+    $wrapped_text .= substr($text, 0, $max_length) . $separator;
+    $text = substr($text, $max_length);
+  }
+  $wrapped_text .= $text;
+  $wrapped_text;
+}
+
+sub wrap_text_in_words {
+  my ($text, $max_length, $separator) = @_;
+  my @words = split / /, $text;
+  my $line = q();
+  my @lines;
+  
+  for my $word (@words) {
+    if (length($line) + length($word) + 1 <= $max_length) {
+      $line .= ($line ? ' ' : '') . $word;
+    } else {
+      push @lines, $line;
+      $line = $word;
+    }
+  }
+  push @lines, $line if $line;
+
+  return join $separator, @lines;
 }
 
 sub import {
@@ -37,25 +68,35 @@ sub import {
 
 =head1 NAME
 
-Text::TinyTemplate::Util - Utility functions for Text::TinyTemplate.
+Text::TinyTemplate::Util - Text::TinyTemplateのためのユーティリティ関数
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =head1 SYNOPSIS
 
   <%= escape_html(string) %>
+  <%= wrap_text_in_chars(string, number, separator) %>
+  <%= wrap_text_in_words(string, number, separator) %>
 
 =head1 DESCRIPTION
 
-C<Text::TinyTemplate::Util> provides utility functions for C<Text::TinyTemplate>.
+C<Text::TinyTemplate::Util>はC<Text::TinyTemplate>で使用できる関数を提供します
 
 =head1 FUNCTIONS
 
 =head2 escape_html
 
-Escape unsafe characters E<amp>, E<lt>, E<gt>, and E<quot> in string.
+文字列（string）に含まれるE<amp>、E<lt>、E<gt>、E<quot>などをエスケープします。
+
+=head2 wrap_text_in_chars
+
+文字列(string)に一定の文字数（number）ごとに区切り文字（separator）を挿入します。
+
+=head2 wrap_text_in_words
+
+C<wrap_text_in_words>と同じですが、文字列（string）に英語のような言語を想定しています。
 
 =head1 AUTHOR
 
